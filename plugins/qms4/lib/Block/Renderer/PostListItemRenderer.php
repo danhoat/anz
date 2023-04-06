9c24d2af08be59d1026e1ef1d2b1f3622182aabf
xuanhoangjvit <nevergiveup.xuanhoang@gmail.com>
2023-04-06 16:35:38 +0700

    udpate time

--- plugins/qms4/lib/Block/Renderer/PostListItemRenderer.php
+++ plugins/qms4/lib/Block/Renderer/PostListItemRenderer.php
@@ -198,24 +198,20 @@ class PostListItemRenderer
 			}
 			$html.='</p>';
 		}
-
-
-
-
-
 		if($show_time){
 			if($item->custom_style == 'recommend_style'){
 				$qms4__timetable = get_post_meta($item->ID, 'qms4__timetable', true);
-				$soon_time = array_shift($qms4__timetable);
-				$late_time = array_pop($qms4__timetable);
-				$from 	= $soon_time['label'];
-				$to 	= $late_time['label'];
+				$from = $to = '';
+				if( is_array($qms4__timetable)){
+					$soon_time = array_shift($qms4__timetable);
+					$late_time = array_pop($qms4__timetable);
+					$from 	= $soon_time['label'];
+					$to 	= $late_time['label'];
+				}
 				$html.= '<span class="time"> 時間: <span class="time-around">'.$from.' ~ '.$to.' </span></span>';
 			} else{
 				$html.= '<span class="time"> '. $item->post_date( 'h:m' ).'</span>';
 			}
-
-
 		}
 
 		$html.='</div>';

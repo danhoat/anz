03a9a07632c6659c98d089d0789aff7fbd3dcf74
xuanhoangjvit <nevergiveup.xuanhoang@gmail.com>
2023-04-06 21:50:11 +0700

    update posts_perpagE

--- themes/fabric/page-fair_calendar.php
+++ themes/fabric/page-fair_calendar.php
@@ -19,83 +19,84 @@ while ( have_posts() ) :
 <?php endwhile; ?>
 
 <?php
-$param['count'] = -1;
-$h_tag         = isset( $args['h_tag'] ) ? $args['h_tag'] : 'h2';
-$list_class    = true;
-$show_date     = true;
-$show_modified = false;
-$show_cat      = true;
-$show_author   = true;
+
 $list_type     = ARKHE_LIST_TYPE;
 $list_type 		= 'card';
-$param = array();
-$param['count'] = 6;
-$param['posts_per_page'] = 6;
 
-
-$list 		= qms4_list( 'fair', $param );
+$args = array(
+	'post_type' 	=> 'fair',
+	'post_status' => 'publish',
+	'posts_per_page' => 20,
+	);
+$slugs = isset($_GET['CAT']) ? $_GET['CAT'] : '';
+if($slugs){
+	$slugs = explode("|", $slugs);
+	$args['tax_query'] =  array(
+		array(
+			'taxonomy' => 'fair__category',
+			'field'    => 'slug',
+			'terms'    => $slugs,
+			'operator' => 'IN',
+		)
+	);
+}
+$query = new WP_Query($args);
 $cur_url 	= get_permalink($post->ID);
 ?>
-	<?php farbic_list_categories_filter($cur_url) ?>
+<?php farbic_list_categories_filter($cur_url) ?>
 	<div class=" l-main__body p-archive">
-
-
 		<ul class="p-postList -type-list">
-			<?php foreach ( $list as $item ) { ?>
-			<?php
-			global $post;
-			$post = get_post($item->ID);
-			?>
+			<?php if( $query->have_posts() ) { ?>
+				<?php while($query->have_posts() ){ ?>
+					<?php $query->the_post();  global $post;  $item = fabric_load_item(); ?>
 
-			<li class="<?php echo esc_attr( trim( 'p-postList__item ' . $list_class ) ); ?>">
-				<a href="<?php the_permalink();?>" class="p-postList__link">
-					<?php
-						Arkhe::get_part( 'post_list/item/thumb', array(
-								'sizes' => 'card' === $list_type ? '(min-width: 600px) 400px, 100vw' : '(min-width: 600px) 400px, 40vw',
-							) );
-					?>
-					<div class="p-postList__body">
-						<?php
-							echo '<' . esc_attr( $h_tag ) . ' class="p-postList__title">';
+					<li class="<?php echo esc_attr( trim( 'p-postList__item ' . $list_class ) ); ?>">
+						<a href="<?php the_permalink();?>" class="p-postList__link">
+							<?php
+								Arkhe::get_part( 'post_list/item/thumb', array(
+										'sizes' => 'card' === $list_type ? '(min-width: 600px) 400px, 100vw' : '(min-width: 600px) 400px, 40vw',
+									) );
 							?>
-						<?php the_title() ; ?>
-						<?php echo '</' . esc_attr( $h_tag ) . '>'; ?>
+							<div class="p-postList__body">
+								<h2 class="p-postList__title"> <?php the_title() ; ?> </h2>
 
-						<div class="p-postList__excerpt"> <?php the_excerpt(); ?></div>
+								<div class="p-postList__excerpt"> <?php the_excerpt(); ?></div>
 
-							<div class="c-postIcon">
-								<?php
-								$categories = get_the_terms( get_the_ID(), 'fair__category' );
+									<div class="c-postIcon">
+										<?php
+										$categories = get_the_terms( get_the_ID(), 'fair__category' );
 
-								if ( $categories && ! is_wp_error( $categories ) ) :
-									$draught_links = array();
-									$html = '';
-									foreach ( $categories as $term ) {
-										$html .= '<li class="icon">'.$term->name.'</li>';
-									}
-									?>
-									<ul class="p-postList__icon"><?= $html ?> </ul>
-								<?php endif; ?>
-								<?php
+										if ( $categories && ! is_wp_error( $categories ) ) :
+											$draught_links = array();
+											$html = '';
+											foreach ( $categories as $term ) {
+												$html .= '<li class="icon">'.$term->name.'</li>';
+											}
+											?>
+											<ul class="p-postList__icon"><?= $html ?> </ul>
+										<?php endif; ?>
+										<?php
 
-								$specials = get_the_terms( get_the_ID(), 'fair__special' );
-								if ( $specials && ! is_wp_error( $specials ) ) :
-									$draught_links = array();
-									$html = '';
-									foreach ( $specials as $term ) {
-										$color = get_field( 'field_62fb7354562ba', $term->taxonomy . '_' . $term->term_id );
-										$bg = 'style = "background-color: '.$color.'" ';
-										$html .= '<li class="icon" '.$bg.'>'.$term->name.'</li>';
-									}
-									?>
-									<ul class="p-postList__icon"><?= $html ?> </ul>
-								<?php endif; ?>
+										$specials = get_the_terms( get_the_ID(), 'fair__special' );
+										if ( $specials && ! is_wp_error( $specials ) ) :
+											$draught_links = array();
+											$html = '';
+											foreach ( $specials as $term ) {
+												$color = get_field( 'field_62fb7354562ba', $term->taxonomy . '_' . $term->term_id );
+												$bg = 'style = "background-color: '.$color.'" ';
+												$html .= '<li class="icon" '.$bg.'>'.$term->name.'</li>';
+											}
+											?>
+											<ul class="p-postList__icon"><?= $html ?> </ul>
+										<?php endif; ?>
 
-						  </div>
-						</div>
-					</a>
-				</li>
+								  </div>
+								</div>
+							</a>
+						</li>
+					<?php }?>
 			<?php } ?>
+			<?php wp_reset_query() ?>
 		  </ul>
 		</div>
 	</main>

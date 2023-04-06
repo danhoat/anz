4788b684ce681f86b934f996a517f922b3d3cdfa
xuanhoangjvit <nevergiveup.xuanhoang@gmail.com>
2023-04-06 08:03:22 +0700

    show date in filter

--- themes/fabric/template-parts/fair/start_content_before.php
+++ themes/fabric/template-parts/fair/start_content_before.php
@@ -1,7 +1,18 @@
+<?php
+ $page_title  ='イベント';
+ $css   = '';
+ $ymd   = isset($_GET['ymd']) ? $_GET['ymd'] :0;
+ if($ymd){
+
+    $date = date_create($ymd);
+    $page_title = 'Date: '. $date->format('y年n月j日');
+    $css = 'calendar-day-list';
+  }
+?>
 <div class="p-topArea -noimg">
   <div class="p-topArea__body l-container">
     <div class="p-topArea__title c-pageTitle">
-      <h1 class="c-pageTitle__main">イベント</h1>
+      <h1 class="c-pageTitle__main <?= $css?>"><?=$page_title ?></h1>
     </div>
   </div>
 </div>

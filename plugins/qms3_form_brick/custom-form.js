468d2e6f5a4771f63d8100254d24513285a2967a
xuanhoangjvit <nevergiveup.xuanhoang@gmail.com>
2023-04-12 08:51:20 +0700

    updat js

--- plugins/qms3_form_brick/custom-form.js
+++ plugins/qms3_form_brick/custom-form.js
@@ -16,52 +16,51 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
 var check = window.location.href.indexOf("reserve_c");
-console.log("reserve_c: ", check);
-if( check > 1){
+
 
 const id = urlParams.get('id');
 
 
-	window.wp.apiFetch( { path: 'wp/v2/fair/'+id+'?enable_schedule_only=1' } ).then(
-    ( result ) => {
-			window.json = result;
-			var qms4__schedules = result.qms4__schedules;
-			var html = '';
-			console.log('qms4__schedules: ', qms4__schedules);
-			var sort = qms4__schedules.sort( ((t, e) => t.date < e.date ? -1 : 1) );
-			console.log('sort: ', sort);
-			qms4__schedules.forEach(function(item, index){
-				html = html + `<option pos="${index}" value="${item.date}" >${(0,l.dateI18n)("n月j日（D）",item.date)}</option>`;
-			});
-
-			dateSelect.html(html);
-			var timehtml = '';
-			qms4__schedules[0].timetable.forEach(function (item, index){
-			if(item.capacity !== '×'){
-				timehtml = timehtml + `<option value = "${item.label}"> ${item.label}</option>`;
-			}
-			})
-
-			window.jQuery("#brick-form__options-fairTime").html(timehtml);
-		},
-		( error ) => {
-			console.log('error: '. error);
-		}
-	);
- 	dateSelect.on("change", (function(t) {
-
-		var element = window.jQuery(this).find('option:selected');
-		var pos 	= element.attr("pos");
-		var timetable = window.json.qms4__schedules[pos].timetable;
+window.wp.apiFetch( { path: 'wp/v2/fair/'+id+'?enable_schedule_only=1' } ).then(
+	( result ) => {
+		window.json = result;
+		var qms4__schedules = result.qms4__schedules;
 		var html = '';
-		timetable.forEach(function(item) {
-
-		if(item.capacity !== '×'){
-			html = html + `<option value = "${item.label}"> ${item.label}</option>`;
-		}
-		window.jQuery("#brick-form__options-fairTime").html(html);
-
+		console.log('qms4__schedules: ', qms4__schedules);
+		var sort = qms4__schedules.sort( ((t, e) => t.date < e.date ? -1 : 1) );
+		console.log('sort: ', sort);
+		qms4__schedules.forEach(function(item, index){
+			html = html + `<option pos="${index}" value="${item.date}" >${(0,l.dateI18n)("n月j日（D）",item.date)}</option>`;
 		});
-  	}));
-}
+
+		dateSelect.html(html);
+		var timehtml = '';
+		qms4__schedules[0].timetable.forEach(function (item, index){
+		if(item.capacity !== '×'){
+			timehtml = timehtml + `<option value = "${item.label}"> ${item.label}</option>`;
+		}
+		})
+
+		window.jQuery("#brick-form__options-fairTime").html(timehtml);
+	},
+	( error ) => {
+		console.log('error: '. error);
+	}
+);
+	dateSelect.on("change", (function(t) {
+
+	var element = window.jQuery(this).find('option:selected');
+	var pos 	= element.attr("pos");
+	var timetable = window.json.qms4__schedules[pos].timetable;
+	var html = '';
+	timetable.forEach(function(item) {
+
+	if(item.capacity !== '×'){
+		html = html + `<option value = "${item.label}"> ${item.label}</option>`;
+	}
+	window.jQuery("#brick-form__options-fairTime").html(html);
+
+	});
+	}));
+
 

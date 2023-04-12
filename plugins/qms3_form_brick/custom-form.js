5f78deacde8e7c004dcdc3433909aded158d8df5
xuanhoangjvit <nevergiveup.xuanhoang@gmail.com>
2023-04-12 13:53:53 +0700

    correct sort schedules

--- plugins/qms3_form_brick/custom-form.js
+++ plugins/qms3_form_brick/custom-form.js
@@ -21,6 +21,10 @@ window.wp.apiFetch( { path: 'wp/v2/fair/'+id+'?enable_schedule_only=1' } ).then(
 		var qms4__schedules = result.qms4__schedules;
 		var html = selected = '';
 
+		console.log('qms4__schedules: ', qms4__schedules);
+
+		qms4__schedules.sort( ((t, e) => t.date < e.date ? -1 : 1) );
+
 		qms4__schedules.forEach(function(item, index){
 			selected = '';
 

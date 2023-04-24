41f579b973f8280eb6e896674a0fc141e97a7b98
Dan Nguyen <danhoat@gmail.com>
2023-04-24 22:25:10 +0700

    show short list schedules in list fair -BE

--- plugins/qms4/lib/Action/Columns/DisplayColumnEventDate.php
+++ plugins/qms4/lib/Action/Columns/DisplayColumnEventDate.php
@@ -68,12 +68,27 @@ class DisplayColumnEventDate
 		asort( $dates );
 
 		$lis = array();
+		$i = 1;
+		$class_css = '';
+		$total = count($dates);
 		foreach ( $dates as $schedule_id => $date ) {
+			$class_css = '';
+			if($i > 10 && $i < $total) $class_css = 'style= "display:none" ';
+
 			$date = EventDate::get_post_meta( $schedule_id );
-			$lis[] = '<li><a href="' . admin_url( "/post.php?post={$schedule_id}&action=edit" ) . '">'
+			$lis[] = '<li  item-'.$i.' '.$class_css.'><a href="' . admin_url( "/post.php?post={$schedule_id}&action=edit" ) . '">'
 				. wp_date( 'Y年n月j日（D）', $date->getTimestamp() )
 				. '</a></li>';
+
+			if( $i == $total -1 && $total > 10 ){
+				$number_hidden = $total - 11;
+				$lis[] = '<li  class="btn_show_full_schedules"> ('.$number_hidden.'  件省略 ... )</li>';
+
+			}
+
+			$i++;
 		}
+
 
 		echo '<ul>' . join( '', $lis ) . '</ul>';
 	}
